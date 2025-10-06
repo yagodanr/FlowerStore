fd47be6972c7272618d80fea601f0ea84288b94a
yagodanr <yagodanr@gmail.com>
2025-10-06 13:36:24 +0300

    Checkstyle:OFF for main

--- src/main/java/ucu/edu/ua/apps/Main.java
+++ src/main/java/ucu/edu/ua/apps/Main.java
@@ -2,7 +2,10 @@ package ucu.edu.ua.apps;
 
 public class Main {
     public static void main(String[] args) {
+        //CHECKSTYLE:OFF
         Flower flower = new Flower(100, FlowerColor.RED, 100, FlowerType.ROSE);
         System.out.println(flower);
+        //CHECKSTYLE:ON
+
     }
 }
